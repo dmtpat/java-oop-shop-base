9680e312d26715b5b2774d34cbc5b13d953018f6
Pat <dmtpatricica@email.com>
2026-04-15 01:29:01 +0200

    construct method

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,21 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Prodotto {
 
+    public int codice;
+    public String nome;
+    public String descrizione;
+    public int prezzo;
+    public int iva;
+
+    public Prodotto(String nome, String descrizione, int prezzo, int iva) {
+        Random rand= new Random();
+        this.codice = rand.nextInt();
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva  = iva;
+    }
 }
