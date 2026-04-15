1c5b9b1432b72afeea2f31c21c2dd2dd676f29d9
Pat <dmtpatricica@email.com>
2026-04-15 02:16:09 +0200

    instance created + method used + printed details on screen

--- src/org/lessons/java/shop/main.java
+++ src/org/lessons/java/shop/main.java
@@ -1,5 +1,13 @@
 package org.lessons.java.shop;
 
-public class main {
-
+public class Main{
+    
+    public static void main(String[] args) {
+    Prodotto Pane = new Prodotto("pane", "Prodotto a uso alimentare a base di farina di grano duro, utilizzato per accompagnare i pasti.", 2.0 , 4);
+                        
+    System.out.println("Il prodotto " + Pane.showFullName() + " ha un prezzo base di " + Pane.showBasePrice()
+            + " euro e un prezzo totale di " + Pane.showFullPrice() + " euro dopo l'aggiunta del " + Pane.iva
+            + "% di iva.");
+    System.out.println( Pane.showFullName());
+    }
 }
